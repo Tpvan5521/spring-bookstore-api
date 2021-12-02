2c08cc499d5b65b43af4b7f575cf16123de32584
tpvan5521 <tpvan5521@gmail.com>
2021-12-02 10:08:32 +0700

    update .gitignore + products-api

--- src/main/java/com/example/bookstoreapi/controller/ProductController.java
+++ src/main/java/com/example/bookstoreapi/controller/ProductController.java
@@ -18,50 +18,54 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class ProductController {
-    
+
     @Autowired
     private ProductService productService;
-    
+
     @GetMapping("/products")
-    public List<Product> getProducts() throws ExecutionException, InterruptedException{
+    public List<Product> getProducts() throws ExecutionException, InterruptedException {
         return productService.getProducts();
     }
-    
+
     @GetMapping("/products/{FIELD}/{value}") // ?type=...
-    public List<Product> getProductsByValue(@PathVariable String FIELD, @PathVariable String value, @RequestParam String type) throws ExecutionException, InterruptedException{
-        if ("number".equals(type) | "Number".equals(type) | "int".equals(type)){
-            return productService.getProductsByValue(FIELD, Integer.parseInt(value));
+    public List<Product> getProductsByValue(@PathVariable String FIELD, @PathVariable String value, @RequestParam String type) throws ExecutionException, InterruptedException {
+        if ("number".equals(type) | "Number".equals(type) | "int".equals(type)) {
+            try {
+                int valueParseToNumber = Integer.parseInt(value);
+                return productService.getProductsByValue(FIELD, valueParseToNumber);
+            } catch (NumberFormatException e) {
+            }
         }
         return productService.getProductsByValue(FIELD, value);
     }
-    
+
     @GetMapping("/products/{FIELD}/{min}/{max}")
-    public List<Product> getProductsByRange(String FIELD, int min, int max) throws ExecutionException, InterruptedException{
+    public List<Product> getProductsByRange(String FIELD, int min, int max) throws ExecutionException, InterruptedException {
         return productService.getProductsByRange(FIELD, min, max);
     }
-    
+
     @GetMapping("/products/order/{FIELD}/limit/{limitValue}")
-    public List<Product> getProductsOrderedAndLimited(String FIELD, int limitValue) throws ExecutionException, InterruptedException{
+    public List<Product> getProductsOrderedAndLimited(String FIELD, int limitValue) throws ExecutionException, InterruptedException {
         return productService.getProductsOrderedAndLimited(FIELD, limitValue);
     }
-    
+
     @GetMapping("/products/{productSlug}")
-    public Product getProductDetails(@PathVariable String productSlug) throws ExecutionException, InterruptedException{
+    public Product getProductDetails(@PathVariable String productSlug) throws ExecutionException, InterruptedException {
         return productService.getProductDetails(productSlug);
     }
-    
+
     @PostMapping("/products")
-    public String createProduct(@RequestBody Product product) throws ExecutionException, InterruptedException{
+    public String createProduct(@RequestBody Product product) throws ExecutionException, InterruptedException {
         return productService.createProduct(product);
     }
-    
+
     @PutMapping("/products/{productSlug}")
-    public String updateProduct(@PathVariable String productSlug, @RequestBody Product product) throws ExecutionException, InterruptedException{
+    public String updateProduct(@PathVariable String productSlug, @RequestBody Product product) throws ExecutionException, InterruptedException {
         return productService.updateProduct(productSlug, product);
     }
-    
+
     @DeleteMapping("/products/{productSlug}")
-    public String deleteProduct(@PathVariable String productSlug) throws ExecutionException, InterruptedException{
+    public String deleteProduct(@PathVariable String productSlug) throws ExecutionException, InterruptedException {
         return productService.deleteProduct(productSlug);
     }
 }
